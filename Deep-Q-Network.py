54bb7bad83865525244d568bb2b6a581b7e398ec
muzian666 <94029822+muzian666@users.noreply.github.com>
2021-11-10 13:53:07 +0800

    Add files via upload

---
+++ Deep-Q-Network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: 199010 Li Qing An
+
+"""
+
+# Deep Q Network 核心代码
+# 导入pytorch做为训练的基本核心
+import torch.nn as nn
+
+class DeepQNetwork(nn.Module):
+    def __init__(self):
+        super(DeepQNetwork, self).__init__()
+
+        self.conv1 = nn.Sequential(nn.Linear(4, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 1))
+# 令AI自主更新 Q-form
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        return x
